a07d208d8265c6e729aed6fb5f406b8bc9142949
Rehman Amjad <rehmanamjid1@gmail.com>
2024-10-01 13:39:49 +0500

    language

--- lib/model/api_services/url/baseurl.dart
+++ lib/model/api_services/url/baseurl.dart
@@ -1,9 +1,11 @@
 class BaseUrl{
- static const API_KEY = "AIzaSyDVxMnFGPxC1TI49JHMYxKCR2BuR9Mj6vU";
+ static const API_KEY = "YOUR_API_KEY";
 
 
+ // your base url
  static const BASEURL_TRANSLATOR = "https://translation.googleapis.com/language/translate/v2?target=";
 
+ // your header
  static const headers = {'Content-Type': 'application/json'};
 
 
